4eeaa14cb2b6a99e415f4de4f964cbb2bee81421
Gongyuan He <poiuyyy@udel.edu>
2017-05-20 21:32:01 +0000

    file upload button2

--- js/script.js
+++ js/script.js
@@ -122,7 +122,6 @@ $(document).ready(function() {
         if(firebaseUser){
           var loggedinHtml=`<i id="googleSignout" style="float: right" class="fa fa-google-plus-official">Hi, ${firebaseUser.email}.Sign out here.</i>`;
           $("#googleSignout").html(loggedinHtml);
-          console.log(firebaseUser);
           $('#googleSignin').hide();
           $("#googleSignout").show();
           $("#comment-add").show();
